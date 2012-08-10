9b76e892e7f14fc1ce99ef318aa4552728e0e546
Eragon Tang <eragon@TYF-PC.(none)>
2012-08-10 14:54:02 +0800

    initial construction with rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HCMMW::Application.config.secret_token = '5f9873933bf8e3e22c456161f9d125d7a61d40e68eb441fce14962132d3b57e18503ebf72876487f75c92e05cb893494ff66ec6b69ac7264629effacf48917d4'
